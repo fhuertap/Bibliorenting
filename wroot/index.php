29c01a710deb93f2eda70c3ea4ebc3bed13812b8
Dafne Dayanira Argueta <dayargueta13@hotmail.com>
2025-02-01 11:14:44 -0600

    prueba

--- wroot/index.php
+++ wroot/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Iniciar Sesión</title>
+    <title>Iniciar Sesión, mexicana</title>
     <link rel="stylesheet" href="_pages/_sources/_styles/login_styles.css">
 </head>
 <body>
