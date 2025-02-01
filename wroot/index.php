bdc8c8bdd273b934576c7a8dcf43de8b1516d32d
Dafne Dayanira Argueta <dayargueta13@hotmail.com>
2025-02-01 11:18:48 -0600

    cambie de ingles a españoll

--- wroot/index.php
+++ wroot/index.php
@@ -8,21 +8,21 @@
 </head>
 <body>
 <div class="login-container">
-    <h2>Login</h2>
+    <h2>Usuario</h2>
     <form action="_pages/layout_page.php" method="POST">
         <div class="form-group">
-            <label for="username">Username</label>
+            <label for="username">Nombre de Usuario</label>
             <input type="text" id="username" name="username" required>
         </div>
         <div class="form-group">
-            <label for="password">Password</label>
+            <label for="password">Contraseña</label>
             <input type="password" id="password" name="password" required>
         </div>
-        <button type="submit" class="login-btn">Login</button>
+        <button type="submit" class="login-btn">Ingresar</button>
     </form>
     <div class="extra-links">
-        <a href="/forgot-password">Forgot Password?</a><br>
-        <a href="/register">Register</a>
+        <a href="/forgot-password">Haz olvidado tu contraseña?</a><br>
+        <a href="/register">Registrar</a>
     </div>
 </div>
 
