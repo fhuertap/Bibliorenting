54966f94e06d108a1485b118e2255dd460985542
Fernando Huerta <b987@outlook.com>
2025-02-03 20:20:19 -0600

    2020

--- wroot/index.php
+++ wroot/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8" lang="Es-es">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iniciar Sesión</title>
     <link rel="stylesheet" href="_pages/_sources/_styles/login_styles.css">
