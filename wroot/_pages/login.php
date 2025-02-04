64d3439f21dd7a7843316fa8fb9473af8d1b456b
Fernando Huawei <b987@outlook.com>
2025-02-04 15:28:26 -0600

    AAAA

---
+++ wroot/_pages/login.php
@@ -0,0 +1,15 @@
+<div class="login-container">
+    <h2>Usuario</h2>
+    <form action="_pages/layout_page.php" method="POST">
+        <div class="form-group">
+            <label for="username">Nombre de Usuario</label>
+            <input type="text" id="username" name="username" required>
+        </div>
+        <div class="form-group">
+            <label for="password">Contraseña</label>
+            <input type="password" id="password" name="password" required>
+        </div>
+        <button type="submit" class="login-btn">Ingresar</button>
+    </form>
+
+</div>
--- wroot/index.php
+++ wroot/index.php
@@ -3,25 +3,13 @@
 <head>
     <meta charset="UTF-8" lang="Es-es">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Iniciar Sesión</title>
+    <title>Bibliorenting</title>
     <link rel="stylesheet" href="_pages/_sources/_styles/login_styles.css">
 </head>
 <body>
-<div class="login-container">
-    <h2>Usuario</h2>
-    <form action="_pages/layout_page.php" method="POST">
-        <div class="form-group">
-            <label for="username">Nombre de Usuario</label>
-            <input type="text" id="username" name="username" required>
-        </div>
-        <div class="form-group">
-            <label for="password">Contraseña</label>
-            <input type="password" id="password" name="password" required>
-        </div>
-        <button type="submit" class="login-btn">Ingresar</button>
-    </form>
-   
-</div>
+<?php
+    include "_pages/login.php"
+?>
 
 </body>
 </html>
