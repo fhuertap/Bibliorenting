e2c2d923a3918dbb9f785e566e6376314f72b7b6
Fernando Huawei <b987@outlook.com>
2025-02-05 13:57:36 -0600

    11

--- wroot/_pages/prueba_data.php
+++ wroot/_pages/prueba_data.php
@@ -1,21 +1,3 @@
 
 <?php
-$serverName = "192.168.2.51"; // e.g., "localhost" or "127.0.0.1"
-$connectionOptions = array(
-    "Database" => "your_database_name",
-    "Uid" => "your_username",
-    "PWD" => "your_password"
-);
 
-// Establishes the connection
-$conn = sqlsrv_connect($serverName, $connectionOptions);
-
-// Checks if the connection was successful
-if ($conn === false) {
-    die(print_r(sqlsrv_errors(), true));
-} else {
-    echo "Connection established.";
-}
-
-// Close the connection when done
-sqlsrv_close($conn);
