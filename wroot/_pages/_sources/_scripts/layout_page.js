2926c62e9fb5cdc35d34c448ab2ecef5e473cf4b
Fernando Huawei <b987@outlook.com>
2025-02-06 10:16:48 -0600

    Agregar botón de cerrar sesión listo

--- wroot/_pages/_sources/_scripts/layout_page.js
+++
@@ -1,9 +0,0 @@
-
-document.getElementById('logoutBtn').addEventListener('click',
-    function (e) {
-        // Prevent default behavior
-        e.preventDefault();
-        // Execute the logout logic
-        alert('Logging out'); // Replace this with actual logic
-        location.href = 'login.php';
-    });
