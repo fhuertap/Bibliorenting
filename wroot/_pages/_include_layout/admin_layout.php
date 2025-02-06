f5941d1a6e7730e30f5c5fe29e5f1fe1a53e9c45
Fernando Huawei <b987@outlook.com>
2025-02-06 13:02:03 -0600

    Agregar botón de cerrar sesión listo

---
+++ wroot/_pages/_include_layout/admin_layout.php
@@ -0,0 +1,7 @@
+<div class="button-container">
+    <button onclick="location.href='page1.php'">Gestionar Libros</button>
+    <button onclick="location.href='page2.php'">Gestión de Alumnos</button>
+    <button onclick="location.href='page3.php'">Gestión de Docentes</button>
+    <button onclick="location.href='page4.php'">Nuevo préstamo de libro</button>
+    <button onclick="location.href='page5.php'">Devolución de libro</button>
+</div>
